10a96acfb1ca84786dbfac69dbdfd4f18d806fd7
KaloyanYosifov <kaloqn665@gmail.com>
2022-06-03 12:52:30 +0200

    Do homework

--- code/week04/src/Week04/Homework.hs
+++ code/week04/src/Week04/Homework.hs
@@ -8,15 +8,16 @@
 
 module Week04.Homework where
 
-import Data.Aeson            (FromJSON, ToJSON)
-import Data.Functor          (void)
-import Data.Text             (Text)
-import GHC.Generics          (Generic)
-import Ledger
-import Ledger.Ada            as Ada
-import Ledger.Constraints    as Constraints
-import Plutus.Contract       as Contract
-import Plutus.Trace.Emulator as Emulator
+import           Data.Aeson            (FromJSON, ToJSON)
+import           Data.Functor          (void)
+import           Data.Text             (Text)
+import           GHC.Generics          (Generic)
+import           Ledger
+import           Ledger.Ada            as Ada
+import           Ledger.Constraints    as Constraints
+import           Plutus.Contract       as Contract
+import           Plutus.Trace.Emulator as Emulator
+import           Wallet.Emulator
 
 data PayParams = PayParams
     { ppRecipient :: PaymentPubKeyHash
@@ -28,7 +29,9 @@ type PaySchema = Endpoint "pay" PayParams
 payContract :: Contract () PaySchema Text ()
 payContract = do
     pp <- awaitPromise $ endpoint @"pay" return
-    let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
+    let recipient = ppRecipient pp
+    let tx = mustPayToPubKey recipient $ lovelaceValueOf $ ppLovelace pp
+    Contract.logInfo @String $ "Paying to " ++ show recipient ++ " amount: " ++ show (ppLovelace pp)
     void $ submitTx tx
     payContract
 
@@ -36,7 +39,17 @@ payContract = do
 -- recipient, but with amounts given by the two arguments. There should be a delay of one slot
 -- after each endpoint call.
 payTrace :: Integer -> Integer -> EmulatorTrace ()
-payTrace _ _ = undefined -- IMPLEMENT ME!
+payTrace fstPay sndPay = do
+    contractHandle <- activateContractWallet (knownWallet 1) payContract
+    callEndpoint @"pay" contractHandle $ PayParams {
+                                                    ppRecipient = mockWalletPaymentPubKeyHash $ knownWallet 2
+                                                   ,ppLovelace = fstPay
+                                                   }
+    void $ Emulator.waitNSlots 1
+    callEndpoint @"pay" contractHandle $ PayParams {
+                                                    ppRecipient = mockWalletPaymentPubKeyHash $ knownWallet 2
+                                                   ,ppLovelace = sndPay
+                                                   }
 
 payTest1 :: IO ()
 payTest1 = runEmulatorTraceIO $ payTrace 10_000_000 20_000_000
